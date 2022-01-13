81317ff03dca0d68e1480049a97ed33c12b04a82
Jiwei Guo <jiwguo@jiwguo-mn2.linkedin.biz>
2022-01-13 14:32:46 -0800

    update

--- src/main/java/com/laioffer/jupiter/ApplicationConfig.java
+++ src/main/java/com/laioffer/jupiter/ApplicationConfig.java
@@ -23,9 +23,9 @@ public class ApplicationConfig {
 
     @Bean(name = "dataSource")
     public DataSource dataSource() {
-        String RDS_ENDPOINT = "database-1.cltdmdpylo37.us-east-2.rds.amazonaws.com";
-        String USERNAME = "admin";
-        String PASSWORD = "12345678";
+        String RDS_ENDPOINT = "";
+        String USERNAME = "";
+        String PASSWORD = "";
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://" + RDS_ENDPOINT + ":3306/twitch?createDatabaseIfNotExist=true&serverTimezone=UTC");
